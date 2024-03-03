b8663b0a14f61589277187514ceae78780a234db
sudhanshu <131753460+sudhanshuprasad236@users.noreply.github.com>
2024-03-03 10:52:29 +0000

    operation added

---
+++ 01-basics/03-conversionoperations.js
@@ -0,0 +1,53 @@
+let score = "33abc"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" =>33
+//"33abc" => nan
+//true => 1; false => 0
+
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+//"manish" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof  stringNumber);
+
+//**********************  operations *********************/
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "manish"
+
+let str3 =  str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+console.log(+true);
+
+let num1 , num2 , num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
